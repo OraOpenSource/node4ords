8ae288aa82804b6d2afb8c8c9c36052397c6e6a1
root <martin.dsouza@gmail.com>
2015-01-30 18:35:09 -0500

    creates public folder

---
+++ scripts/preinstall.js
@@ -0,0 +1,20 @@
+//Create public directory as a starting point for static files
+var fs = require('fs');
+var dir = './public';
+
+if (!fs.existsSync(dir)){
+  fs.mkdirSync(dir);
+}
+
+var
+subDirs = ['js','img', 'css'],
+subDir;
+
+for (i=0; i < subDirs.length; i++){
+  subDir = dir + '/' + subDirs[i];
+  console.log(subDir);
+  if (!fs.existsSync(subDir)){
+    fs.mkdirSync(subDir);
+  }
+
+}
