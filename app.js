8f6fc4df26dfa6c9f819adbe6e8f9bc69c47e31f
root <martin.dsouza@gmail.com>
2015-01-31 14:49:43 -0500

    Updates

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var favicon = require('serve-favicon');
-var proxy   = require( 'http-proxy' ).createProxyServer({});
+var proxy = require( 'http-proxy' ).createProxyServer({});
 var http = require('http');
 //var https = require('https');
 
-var httpPort = process.env.PORT || '80';
-var httpsPort = process.env.HTTPS || '443';
+var ports = {
+  http: process.env.PORT || '80',
+  https: process.env.PORT || '443'
+}
 
+var tomcatUrl = 'http://localhost:8080';
 var app = express();
 
 
@@ -19,54 +22,21 @@ app.use('/apex',function(req, res, next){
 });
 
 //Can store custom images in public/...
-app.use('/public',express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 app.use('/i',express.static('/usr/share/apache-tomcat-7.0.57/webapps/i/'));
 
-app.use( ['/ords','/itodo'], function (req, res, next){
-  //Will only use /i/ from tomcat if app.use('/i') is commented out above
-  proxy.web(req, res, { target: 'http://localhost:8080' + req.baseUrl});
+app.use('/ords', function (req, res, next){
+  proxy.web(req, res, { target: tomcatUrl + req.baseUrl});
 });
 
-//Make sure this is last as it will forward to APEX
+// Make sure this is last as it will forward to APEX
 app.use(function(req, res, next){
   res.redirect('/ords');
 });
 
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-
-
-//module.exports = app;
 // app.listen(httpPort);
-http.createServer(app).listen(80);
+http.createServer(app).listen(ports.http);
 //Enable once SSL cert defined
-// https.createServer(options, app).listen(443);
+// https.createServer(options, app).listen(ports.https);
