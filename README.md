1f097ad25207cd40b7d3e58ea779346aaca5bc57
Martin D'Souza <martin.dsouza@gmail.com>
2016-09-07 21:45:39 -0600

    #2 SSL support

--- README.md
+++ README.md
@@ -23,8 +23,32 @@ Node4ORDS comes with a self updating script. If you have made any customizations
 
 If you are missing `update.sh` it means you are running an older version of Node4ORDS. Please do a manual upgrade as described in [this post](http://www.oraopensource.com/blog/2015/4/18/node4ords-update).
 
-#Config
-All the configuration in ```config.js```. This should be reviewed before installing to configure the web container URL, ports, etc.
+# Config
+The configuration for node4ords is stored in `config.js`. When installing Node4ORDS the first time this file will be created as a copy of [`defaultConfig.js`](defaultConfig.js).
+
+The following is an explanation of all the settings
+
+Setting | Required | Default | Description
+--- | ---
+`config.web.http` | | |  HTTP config container
+`config.web.http.port` | optional | `80` |  Port for http connections
+`config.web.https` | optional | |  If defined, will trigger HTTPS option
+`config.web.https.port` | optional | `443` |  Port for https connections
+`config.web.https.keyPath` | required | | Full path to SSL private key _Required only if HTTPS is enabled_
+`config.web.https.certPath` | required | | Full path to SSL certificate _Required only if HTTPS is enabled_
+`config.web.https.forceHttps` | optional | `false` | If `true` then all HTTP connections will be redirected to HTTPS
+`config.web.https.forceSSLPort` | optional | `config.web.https.port` | If `config.web.https.forceHttps` is enabled then this port will be used for the redirect. In most cases `443` (the default) is appropriate. A different port is usually only required if running Node4ORDS on a VM and doing port mapping that maps `443` to a different (local) port.
+`config.ords` | | | ORDS config container
+`config.ords.path` | required | | URL path for ORDS. Recommended `/ords`
+`config.ords.redirectPaths` | optional | | Array of paths to redirect. Ex: `['/apex']` will redirect all references to `/apex` to path set in `config.ords.path`. Useful for older URLs that may have used `/apex` or other paths to access APEX applications.
+`config.ords.webContainerUrl` | required | | local URL to Tomcat server. Most of the time this will be `http://localhost:8080`
+`config.apex` | | | APEX config container
+`config.apex.images` | required | | APEX images container
+`config.apex.images.path` | required |  | URL Path to APEX images. Recommended `/i`
+`config.apex.images.directory` | required |  | Path on file system where APEX images are located. Ex: `/ords/apex_images`
+`config.static` | | | Static www files container
+`config.static.path` | | | Path to access static www files. Ex `/public`
+`config.static.directory` | | | Filesystem location of where www static files are stored. Ex: `/var/www/public`
 
 #Run
 
@@ -35,10 +59,10 @@ node app.js
 An example of how to start this app on boot can be found in the [Oraclexe-Apex](https://github.com/OraOpenSource/oraclexe-apex) project. Specifically the [init.d/node4ords](https://github.com/OraOpenSource/oraclexe-apex/blob/master/init.d/node4ords) file.
 
 #Static files
-Once installed a directory called ```/var/www/public``` will be created. Static files can be placed in there and referenced from your server via *//server_name/public/filename*. You can configure the location for static content in ```config.js```.
+Once installed a directory called `/var/www/public` will be created. Static files can be placed in there and referenced from your server via `//server_name/public/filename`. You can configure the location for static content in ```config.js```.
 
 By default, the following folder structure will be created:
-```
+```bash
 |-/public
 	|-css
 	|-img
--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var favicon = require('serve-favicon');
-var proxy = require( 'http-proxy' ).createProxyServer({});
 var http = require('http');
-//var https = require('https');
-
+var https = require('https');
+var forceSSL = require('express-force-ssl');
 var app = express();
-
+var proxy = require('http-proxy-middleware');
 
 var config = require('./config');
+var privateKey, certificate;
+
+// Default Ports
+var PORTS = {
+  HTTP: config.web.http.port || 80,
+  HTTPS: config.web.https.port || 443,
+  FORCE_SSL_PORT: ''
+}
+
+PORTS.FORCE_SSL_PORT = config.web.https.forceSSLPort || PORTS.HTTPS;
+
+// #2 SSL Support. All triggerd by the presence of config.web.https
+if (config.web.https){
+  console.log('Enabling HTTPS');
+  privateKey = fs.readFileSync(path.resolve(config.web.https.keyPath));
+  certificate = fs.readFileSync(path.resolve(config.web.https.certPath));
+
+  // TODO mdsouza: make the httpsPort modifiable
+  if (config.web.https.forceHttps) {
+    app.set('forceSSLOptions', {
+      enable301Redirects: true,
+      trustXFPHeader: false,
+      httpsPort: PORTS.FORCE_SSL_PORT,
+      sslRequiredMessage: 'SSL Required.'
+    });
+    app.use(forceSSL);
+  }// config.web.https.forceHttps
+
+}// config.web.https
+
 
 //Uncomment if you don't want to redirect / and /apex to the new /ords
 if (config.ords.redirectPaths.length > 0){
@@ -28,23 +59,56 @@ app.use(config.apex.images.path,express.static(config.apex.images.directory));
 //   app.use(favicon(__dirname + config.faviconUrl));
 // }
 
+// https://github.com/chimurai/http-proxy-middleware
 
-app.use(config.ords.path, function (req, res, next){
-  proxy.web(req, res, { target: config.ords.webContainerUrl + req.baseUrl},
-    function(e){
-      console.log('proxy.web error: ', e);
-    }
-  );
-});
+app.use(config.ords.path,proxy(
+  {
+    target: config.ords.webContainerUrl,
+    changeOrigin: false,
+    // Additional work seems to be required for unsigned certificats
+    onProxyReq: function(proxyReq, req, res) {
+      // For encrypted calls, if we don't set the origin on POST request then we'll get the following error
+      // The request cannot be processed because this resource does not support Cross Origin Sharing requests, or the request Origin is not authorized to access this resource. If ords is being reverse proxied ensure the front end server is propagating the host name, for mod_proxy ensure ProxyPreserveHost is set to On
+      if (req.connection.encrypted && req.headers.origin){
+        proxyReq.setHeader('origin', req.headers.origin.replace(/^https:/,'http:'));
+      }
+    }, //onProxyReq
+    onProxyRes: function(proxyRes, req, res){
+      // If encrypted and headers['location'] exists (doesn't happen on some redirects)
+      if (req.connection.encrypted && proxyRes.headers['location']){
+        proxyRes.headers['location'] = proxyRes.headers['location'].replace(/^http:/g,'https:');
+      }
+    } // onProxyRes
+  }
+));
+
+// app.use(config.ords.path, function (req, res, next){
+//   console.log('***');
+//   console.log('in app.use(config.ords.path before proxy');
+// });
+
+// proxy.on('proxyReq', function(proxyReq, req, res, options) {
+//   console.log('hello');
+// });
 
 // Make sure this is last as it will forward to APEX
 app.get('/', function(req, res, next){
+  console.log('in / forward');
+  // console.log('req.headers.origin:', req.headers);
   res.redirect(config.ords.path);
 });
 
 
 
+
 // app.listen(httpPort);
-http.createServer(app).listen(config.web.http.port);
-//Enable once SSL cert defined
-// https.createServer(options, app).listen(config.ports.https);
+http.createServer(app).listen(PORTS.HTTP);
+
+if (config.web.https){
+  https.createServer(
+    {
+      key: privateKey,
+      cert: certificate
+    },
+    app).listen(PORTS.HTTPS);
+}// config.web.https
